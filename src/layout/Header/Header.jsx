01d419b78e2a9315617c18224aeba7b0b312705c
yogesh-karki <kulchan.yogesh2@gmail.com>
2024-04-24 10:40:38 +0545

    header

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Grid } from "@mui/material";
 import headerStyle from "./header.module.scss";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -18,20 +19,63 @@ const Header = () => {
           <Grid item md={7}>
             <ul className={headerStyle.nav}>
               <li>
-                <a href="">About</a>
+                <a href="#">About</a>
+
+                <div className={headerStyle.submenu}>
+                  <div className={headerStyle.wrap}>
+                    <Link href={"/"}>Focus Area</Link>
+                    <Link href={"/"}>Impacts</Link>
+                    <Link href={"/"}>Team</Link>
+                  </div>
+                </div>
               </li>
               <li>
-                <a href="">CREASION</a>
+                <a href="#">CREASION</a>
+
+                <div className={headerStyle.submenu}>
+                  <div className={headerStyle.wrap}>
+                    <Link href={"/"}>
+                      Strengthening of existing private waste enterprises
+                    </Link>
+                    <Link href={"/"}>Empowering WWs through CSOs </Link>
+                    <Link href={"/"}>
+                      Provincial SWM policy guideline preparation and handover{" "}
+                    </Link>
+                    <Link href={"/"}>Sustainable packaging program </Link>
+                    <Link href={"/"}>Green business accelerator program </Link>
+                  </div>
+                </div>
               </li>
               <li>
-                <a href="">Restless Development</a>
+                <a href="#">Restless Development</a>
+                <div className={headerStyle.submenu}>
+                  <div className={headerStyle.wrap}>
+                    <Link href={"/"}>
+                      Establishment of Climate Smart Schools at 36 schools
+                    </Link>
+                    <Link href={"/"}>Youth Advocacy </Link>
+                  </div>
+                </div>
               </li>
               <li>
-                <a href="">Youth Innovation Lab</a>
+                <a href="#">Youth Innovation Lab</a>
+                <div className={headerStyle.submenu}>
+                  <div className={headerStyle.wrap}>
+                    <Link href={"/"}>Waste Smart Fellowship</Link>
+                    {/* <Link href={"/"}>Tag me app</Link> */}
+                  </div>
+                </div>
               </li>
 
               <li>
-                <a href="">Knowledge Hub</a>
+                <a href="#">Knowledge Hub</a>
+                <div className={headerStyle.submenu}>
+                  <div className={headerStyle.wrap}>
+                    <Link href={"/"}>Blogs & Field Stories </Link>
+                    <Link href={"/"}>Publications & Reports </Link>
+                    <Link href={"/"}>News & Updates </Link>
+                  </div>
+                </div>
               </li>
             </ul>
           </Grid>
@@ -39,9 +83,18 @@ const Header = () => {
           <Grid item md={3}>
             <ul className={`${headerStyle.nav} ${headerStyle.nav_align_right}`}>
               <li>
-                <a href="" className={headerStyle.btn_highlight}>
+                <a href="#" className={headerStyle.btn_highlight}>
                   Join the Green Movement
                 </a>
+
+                <div className={headerStyle.submenu}>
+                  <div className={headerStyle.wrap}>
+                    <Link href={"/"}>What is the Green Movement? </Link>
+                    <Link href={"/"}>(Realistic) Pledge </Link>
+                    <Link href={"/"}>GreenShift Campaign </Link>
+                    <Link href={"/"}>Pitch Your Ideas </Link>
+                  </div>
+                </div>
               </li>
             </ul>
           </Grid>
