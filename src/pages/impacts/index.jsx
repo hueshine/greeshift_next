a182d2cfb963e9c7a498ef0b4accde0bf5b7f3c0
yogesh-karki <kulchan.yogesh2@gmail.com>
2024-05-29 15:32:27 +0545

    console log removed from impact

--- src/pages/impacts/index.jsx
+++ src/pages/impacts/index.jsx
@@ -3,7 +3,6 @@ import MapConponent from "../../components/MapComponent/MapComponent";
 import Head from "next/head";
 
 const Impacts = ({ apiData }) => {
-  console.log(apiData);
   return (
     <>
       <Head>
